f1474ca9f16c4921af4c3118f223f11613bb6ae6
Marcin Makarewicz <marcin@makarewicz.eu>
2021-05-25 20:45:37 +0200

    feat: add c++ addon

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -1,1 +1,8 @@
-export default (text: string) => `Hello ${text}`;
+const addon = require("../build/Release/addon");
+
+// https://nodejs.org/api/addons.html
+// https://koistya.medium.com/how-to-call-c-c-code-from-node-js-86a773033892
+// https://github.com/kriasoft/graphql-starter/tree/c98520681f7511f003a387c14e7c8d3fecf836f2/src/utils
+// https://stackoverflow.com/questions/44363573/how-to-access-so-library-from-nodejs-application
+// https://medium.com/@HolmesLaurence/integrating-node-and-python-6b8454bfc272
+export default (text: string) => addon.hello(text);
